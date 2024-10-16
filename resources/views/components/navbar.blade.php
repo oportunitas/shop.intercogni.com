184760241a6402fffa8a2fa2a1b95e058b249f0c
oportunitas <taibizzat165@gmail.com>
2024-10-17 00:55:33 +0700

    💄 style: add intercogni logo to navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,16 +1,18 @@
-<nav class=" border-gray-200 px-4 lg:px-6 py-2.5 bg-white">
-    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-        
-        <a href="#" class="flex ">
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">shop@intercogni</span>
+<nav class="border-gray-900 px-4 lg:px-6 py-2.5 bg-gray-800">
+    <div class="bg-gray-800 flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+        <div class="flex items-center">
+            <img src="itr_cgi.png" alt="Logo" class="rounded-lg h-10 w-10 object-cover">
+        </div>
+
+        <a href="#" class="flex">
+            <span class="self-center text-xl font-semibold whitespace-nowrap text-slate-300">shop@intercogni</span>
         </a>
         
         <div class="text-center">
-        <button id="createProductButton" class="bg-indigo-600 text-white hover:bg-red-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800" type="button" data-drawer-target="drawer-create-product-default" data-drawer-show="drawer-create-product-default" aria-controls="drawer-create-product-default">
-        Create product
+        <button id="createProductButton" class="bg-red-600 text-white hover:bg-orange-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none" type="button" data-drawer-target="drawer-create-product-default" data-drawer-show="drawer-create-product-default" aria-controls="drawer-create-product-default">
+            Create product
         </button>
     </div>
 </nav>
-
 
 
